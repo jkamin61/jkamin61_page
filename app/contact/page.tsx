d63b3c9f46c63148253f27cad8c5af82993f2484
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-11-20 13:18:49 +0100

    small margin fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -39,13 +39,13 @@ export default function Contact() {
             className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden dark-theme relative">
             <Navigation/>
             <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-                <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+                <div className="grid w-full grid-cols-1 gap-8 mx-auto sm:mt-0 sm:grid-cols-3 lg:gap-16">
                     {socials.map((s) => (
                         <Card key={s.label}>
                             <Link
                                 href={s.href}
                                 target="_blank"
-                                className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+                                className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16"
                             >
 								<span
                                     className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
