447b11bc0dd2798bdb61174bea44170b1e224ce5
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-11-20 13:19:25 +0100

    template for projects list

---
+++ app/projects/individual-projects/page.tsx
@@ -0,0 +1,14 @@
+import Navigation from "../../components/nav";
+import React from "react";
+
+const IndividualProjectsPage = () => {
+    return (
+        <div
+            className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden dark-theme relative">
+            <Navigation/>
+            <div></div>
+        </div>
+    );
+}
+
+export default IndividualProjectsPage;
