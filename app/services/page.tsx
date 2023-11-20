637f366cae601ac42e88c5faeb206732ac94b679
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-11-20 13:20:25 +0100

    template for services page

---
+++ app/services/page.tsx
@@ -0,0 +1,16 @@
+//TODO: Page with fiverr gigs
+
+import React from "react";
+import Navigation from "../components/nav";
+
+const Services: React.FC = () => {
+    return (
+        <div
+            className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden dark-theme relative">
+            <Navigation/>
+            <div></div>
+        </div>
+    )
+}
+
+export default Services;
