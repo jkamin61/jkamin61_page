ad61b9d2c2985ef606acb9170a8b3721973cebc0
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-11-21 21:29:53 +0100

    small fix// switched contact and services link place, added backdrop-blur

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -17,14 +17,14 @@ const Navigation: React.FC = () => {
         observer.observe(ref.current);
         return () => observer.disconnect();
     }, []);
-
+    
     return (
         <header ref={ref}>
             <div
-                className={`fixed inset-x-0 top-0 z-50  duration-200 border-b  ${
+                className={`fixed inset-x-0 top-0 z-50 backdrop-blur duration-200 border-b  ${
                     isIntersecting
                         ? "bg-zinc-900/0 border-transparent"
-                        : "bg-zinc-900/500  border-zinc-800 "
+                        : "bg-zinc-900/500 border-zinc-800"
                 }`}
             >
                 <div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
@@ -36,16 +36,16 @@ const Navigation: React.FC = () => {
                             Projects
                         </Link>
                         <Link
-                            href="/contact"
-                            className="duration-200 text-zinc-500 hover:text-zinc-100"
-                        >
-                            Contact
-                        </Link>
-                        <Link
                             href="/services"
                             className="duration-200 text-zinc-500 hover:text-zinc-100"
                         >
                             Services
+                        </Link>
+                        <Link
+                            href="/contact"
+                            className="duration-200 text-zinc-500 hover:text-zinc-100"
+                        >
+                            Contact
                         </Link>
                     </div>
 
